cd643c825b93e4abcd6d8f848e90a8b02a2e69d6
MartinVillodas <58822330+MartinVillodas@users.noreply.github.com>
2020-09-19 10:55:57 -0300

    Initial commit

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -11,35 +11,37 @@ Programa creado para poner a prueba los conocimientos
 adquiridos durante la clase
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
+__author__ = "Martin Villodas"
+__email__ = "martinvillodas@gmail.com"
 __version__ = "1.2"
 
 import csv
 import re
 
+def contar_lineas(nombre):
+    cantidad_lineas = 0
+    for linea in nombre:
+        cantidad_lineas += 1
+    return cantidad_lineas
+
 
 def ej1():
     # Ejercicios con archivos txt
-    cantidad_lineas = 0
 
     '''
     Realizar un prorgrama que cuenta la cantidad de líneas
     de un archivo. Abra el archivo "notas.txt" en modo "lectura",
     lea linea a linea el archivo, y cuente la cantidad de líneas.
     Al finalizar el proceso, imprimir en pantalla la cantidad
-    de líneas leaidas.
+    de líneas leidas.
 
     Como práctica de funciones, puede realizar la función
     "contar_lineas" que reciba como parámetro el nombre del archivo
     y cumpla el objetivo especificado, retornando la cantidad
     de líneas encontradas.
     '''
-    
-    fi = open("notas.txt", "r")
-
-    fi.close()
-
+    with open("notas.txt","r") as fi:
+        print(contar_lineas(fi))
 
 
 def ej2():
@@ -59,16 +61,22 @@ def ej2():
     contar la cantidad de línea que se copiaron e imprimir
     al final del proceso el valor.
     '''
+    fi = open("notas.txt","r")
+    fo = open("copia_nota.txt","w")
 
-    # fi = open('nota.txt', 'r')
-    # fo = open(.......)
+    for linea in fi:
+        cantidad_lineas_fo = 0
+        fo.writelines(linea)
+        cantidad_lineas_fo += contar_lineas(fo)
+
+    fi.close()
+    fo.close()
 
     # Recuerde cerrar los archivos al final ;)
 
 
 def ej3():
     # Ejercicios con archivos CSV
-    archivo = 'propiedades.csv'
     '''
     Realice un programa que abra el archivo CSV "propiedades.csv"
     en modo lectura. Recorrar dicho archivo y contar
@@ -76,75 +84,13 @@ def ej3():
     de departamentos de 3 ambientes disponibles.
     Al finalizar el proceso, imprima en pantalla los resultados.
     '''
-
-
-def ej4():
-    # Ejercicios con diccionarios
-    inventario = {'manzanas': 6}
-
-    '''
-    Realice un programa que pida por consola
-    el nombre de una fruta o verdura y luego
-    pida la cantidad que hay en stock
-    Agregar al diccionario "inventario" el par:
-    <fruta>:<cantidad>
-    El diccionario "inventario" ya viene cargado
-    con el valor el stock de manzanas para que vean
-    de ejemplo.
-    Esta operacion se debe realizar en un bucle
-    hasta ingresar como fruta/verdura la palabra "FIN"
-
-    '''
-
-    # En el bucle realizar:
-    # Generar y completar el diccionario con las frutas y cantidades
-    # ingresadas por consola hasta ingresar la palabra "FIN"
-
-
-def ej5():
-    # Ejercicios con archivos CSV
-    inventario = {'Fruta Verdura': 'manzana', 'Cantidad': 10}
-
-    '''
-    Parecido al el ejercicio anterior, genere un archivo CSV
-    (abrir un archivo CSV como escritura) que posea las siguientes
-    columnas:
-    1) 'Fruta Verdura'
-    2) 'Cantidad'
-
-    Estas dos columnas representan el nombre de las dos "claves"
-    del diccionario, que utilizaremos para escribir en el archivo CSV:
-
-    writer.writerow({'Fruta Verdura': ....., 'Cantidad': ....})
-
-    Ojo! No es igual al diccionario del anterior ejercicio, 
-    porque el anterior usaba como "clave" el nombre de la fruta.
-    Ahora tenemos dos pares de valores "clave: valor", pueden
-    ver el inventario con el ejemplo de la manzana.
-
-    Deberá realizar un bucle en donde en cada iteracion del bucle
-    se le socilitará por consola que ingrese un tipo de fruta o verdura
-    y su cantidad, deberá escribir una línea en el archivo CSV (una fila)
-    con esa información ingresada.
-    El bucle finalizará cuando se ingrese como fruta o verdura
-    la palabra "FIN"
-
-    Al finalizar deberá tener un archivo (con el nombre que usted haya
-    elegido) con todas las filas completas en las dos columnas especificadas
-    con todas las frutas o verduras ingresadas y sus cantidades
-    '''
-    # Recuerde crear el header correspondiente con "writeheader", el cual
-    # se debe especificar al abrir el archivo.
-
-    # Bucle....
-
-    # writer.writerow({'Fruta Verdura': ....., 'Cantidad': ....})
-
+    archivo = 'propiedades.csv'
+    with open(archivo,"r") as propiedades:
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    ej1()
-    #ej2()
+    #ej1()
+    ej2()
     #ej3()
     #ej4()
     #ej5()
